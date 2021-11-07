f638aeb63f9c26c9e4031f01113b0655f7dc5f3f
AdrielDatario <79733895+AdrielDatario@users.noreply.github.com>
2021-11-06 18:53:21 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -2,12 +2,14 @@ video = "";
 status = "";
 
 function preload(){
-    video = createVideo('video.mp4')
+    //video = createVideo('video.mp4')
 }
 function sutup(){
     canvas = createCanvas(480, 380);
     canvas.center();
-    video.hide();
+    video = createCapture(video);
+    video.size(480, 380);
+    //video.hide();
 }
 
 function start(){
@@ -21,4 +23,8 @@ function modelLoaded(){
     video.loop();
     video.speed(1);
     video.volume(0);
+}
+
+function draw(){
+    image(video , 0, 0, 480, 380);
 }
